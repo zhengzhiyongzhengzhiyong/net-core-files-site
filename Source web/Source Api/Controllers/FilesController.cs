ae3d80fa5b94789971c3db761952c64b4206ca29
志勇 郑 <zhengzhiyongzhengzhiyong@hotmail.com>
2020-07-10 10:42:48 +0800

    recreated

---
+++ Source web/Source Api/Controllers/FilesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Source_Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly IHostingEnvironment hostingEnvironment;
+        public FilesController(IHostingEnvironment environment)
+        {
+            hostingEnvironment = environment;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            try
+            {
+                if (file == null || file.Length == 0)
+                    return Content("file not selected");
+
+                var uniqueFileName = GetUniqueFileName(file.FileName);
+                string date = DateTime.Now.ToString("yyyy-MM-dd");
+                var dirpath = Path.Combine(hostingEnvironment.ContentRootPath, "uploads", date);
+
+                if (!Directory.Exists(dirpath))
+                {
+                    Directory.CreateDirectory(dirpath);
+                }
+
+                var filePath = Path.Combine(dirpath, uniqueFileName);
+
+                if (file.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+
+
+
+                string webfileurl = $"http://localhost:58444/uploads/{date}/{uniqueFileName}";
+
+                return Ok(new { path=webfileurl});
+            }
+            catch (Exception exp)
+            {
+                string message = $"file / upload failed!";
+                return Ok(message);
+            }
+        }
+
+        private string GetUniqueFileName(string fileName)
+        {
+            fileName = Path.GetFileName(fileName);
+            return Guid.NewGuid().ToString() + Path.GetExtension(fileName);
+        }
+    }
+}
---
+++ Source web/Source Api/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Source_Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllRequests", policy =>
+                {
+                    policy
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+                });
+
+                c.AddPolicy("LimitRequests", policy =>
+                {
+                    policy
+                    .WithOrigins("http://127.0.0.1:1818", "http://localhost:1818")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors("AllRequests");
+            app.UseMvc();
+        }
+    }
+}
